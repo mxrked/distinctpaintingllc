d8f6738b8662b2ae0f3d9e1a7cbde75004cc7c3b
Parker <pp101socials@gmail.com>
2023-08-21 08:48:12 -0400

    Added cdns

--- assets/cdns/CDNBgs.js
+++ assets/cdns/CDNBgs.js
@@ -6,4 +6,39 @@
 
 import { CDNBGReturn } from "./CDNReturns";
 
-export {  };
+// INDEX
+const INDEX_TOP_BG_1 = CDNBGReturn("index", "top-bg-1", "webp");
+const INDEX_TOP_BG_2 = CDNBGReturn("index", "top-bg-2", "webp");
+const INDEX_TOP_BG_3 = CDNBGReturn("index", "top-bg-3", "webp");
+const INDEX_TOP_BG_4 = CDNBGReturn("index", "top-bg-4", "webp");
+const INDEX_PAINTING_BG = CDNBGReturn("index", "painting-bg", "webp");
+const INDEX_PRESSURE_WASHING_BG = CDNBGReturn(
+  "index",
+  "pressure-washing-bg",
+  "webp"
+);
+const INDEX_DRY_WALL_REPAIR_BG = CDNBGReturn("index", "dry-wall-bg", "webp");
+const INDEX_CARPENTRY_BG = CDNBGReturn("index", "carpentry-bg", "webp");
+const INDEX_ABOUT_BG = CDNBGReturn("index", "about-bg", "webp");
+const INDEX_CONTACT_BG = CDNBGReturn("index", "contact-bg", "webp");
+
+// GALLERY
+const GALLERY_TOP_BG = CDNBGReturn("gallery", "carpentry-1-min", "webp");
+
+// CONTACT
+const CONTACT_TOP_BG = CDNBGReturn("contact", "painter-1-min", "webp");
+
+export {
+  INDEX_ABOUT_BG,
+  INDEX_CARPENTRY_BG,
+  INDEX_CONTACT_BG,
+  INDEX_DRY_WALL_REPAIR_BG,
+  INDEX_PAINTING_BG,
+  INDEX_PRESSURE_WASHING_BG,
+  INDEX_TOP_BG_1,
+  INDEX_TOP_BG_2,
+  INDEX_TOP_BG_3,
+  INDEX_TOP_BG_4,
+};
+export { GALLERY_TOP_BG };
+export { CONTACT_TOP_BG };
--- assets/cdns/CDNImgs.js
+++ assets/cdns/CDNImgs.js
@@ -6,28 +6,168 @@
 
 import { CDNImgReturn } from "./CDNReturns";
 
-const PLACEHOLDER = CDNImgReturn("dogs", "placeholder", "png");
-const PLACEHOLDER_BLUE = ("placeholders", "blue", "webp");
-const PLACEHOLDER_RED = ("placeholders", "red", "webp");
 const PLACEHOLDER_GREEN = ("placeholders", "green", "webp");
-const PLACEHOLDER_PURPLE = ("placeholders", "purple", "webp");
 
-const MERLIN_1 = CDNImgReturn("dogs", "merlin-1", "webp");
-const PHOENIX_1 = CDNImgReturn("dogs", "phoenix-1", "webp");
-const PHOENIX_2 = CDNImgReturn("dogs", "phoenix-2", "webp");
-const SOPHIE_1 = CDNImgReturn("dogs", "sophie-1", "webp");
-const SOPHIE_2 = CDNImgReturn("dogs", "sophie-2", "webp");
-const SOPHIE_3 = CDNImgReturn("dogs", "sophie-3", "webp");
+const OUTSIDE_IMG_1 = CDNImgReturn("gallery", "outside-1", "webp");
+const OUTSIDE_IMG_2 = CDNImgReturn("gallery", "outside-2", "webp");
+const OUTSIDE_IMG_3 = CDNImgReturn("gallery", "outside-3", "webp");
+const OUTSIDE_IMG_4 = CDNImgReturn("gallery", "outside-4", "webp");
+const OUTSIDE_IMG_5 = CDNImgReturn("gallery", "outside-5", "webp");
+const OUTSIDE_IMG_6 = CDNImgReturn("gallery", "outside-6", "webp");
+const OUTSIDE_IMG_7 = CDNImgReturn("gallery", "outside-7", "webp");
+const OUTSIDE_IMG_8 = CDNImgReturn("gallery", "outside-8", "webp");
+const OUTSIDE_IMG_9 = CDNImgReturn("gallery", "outside-9", "webp");
+const OUTSIDE_IMG_10 = CDNImgReturn("gallery", "outside-10", "webp");
+const OUTSIDE_IMG_11 = CDNImgReturn("gallery", "outside-11", "webp");
+const OUTSIDE_IMG_12 = CDNImgReturn("gallery", "outside-12", "webp");
+const OUTSIDE_IMG_13 = CDNImgReturn("gallery", "outside-13", "webp");
+const OUTSIDE_IMG_14 = CDNImgReturn("gallery", "outside-14", "webp");
+const OUTSIDE_IMG_15 = CDNImgReturn("gallery", "outside-15", "webp");
+const OUTSIDE_IMG_16 = CDNImgReturn("gallery", "outside-16", "webp");
+const OUTSIDE_IMG_17 = CDNImgReturn("gallery", "outside-17", "webp");
+const OUTSIDE_IMG_18 = CDNImgReturn("gallery", "outside-18", "webp");
+const OUTSIDE_IMG_19 = CDNImgReturn("gallery", "outside-19", "webp");
+const OUTSIDE_IMG_20 = CDNImgReturn("gallery", "outside-20", "webp");
+const OUTSIDE_IMG_21 = CDNImgReturn("gallery", "outside-21", "webp");
+const OUTSIDE_IMG_22 = CDNImgReturn("gallery", "outside-22", "webp");
+const OUTSIDE_IMG_23 = CDNImgReturn("gallery", "outside-23", "webp");
+const OUTSIDE_IMG_24 = CDNImgReturn("gallery", "outside-24", "webp");
+const OUTSIDE_IMG_25 = CDNImgReturn("gallery", "outside-25", "webp");
+const OUTSIDE_IMG_26 = CDNImgReturn("gallery", "outside-26", "webp");
+const OUTSIDE_IMG_27 = CDNImgReturn("gallery", "porch-1", "webp");
+const OUTSIDE_IMG_28 = CDNImgReturn("gallery", "porch-2", "webp");
 
-const DOG_ART_1 = CDNImgReturn("dog-art", "dog-art-1", "webp");
-const DOG_ART_2 = CDNImgReturn("dog-art", "dog-art-2", "webp");
+const ROOM_IMG_1 = CDNImgReturn("gallery", "room-1", "webp");
+const ROOM_IMG_2 = CDNImgReturn("gallery", "room-2", "webp");
+const ROOM_IMG_3 = CDNImgReturn("gallery", "room-3", "webp");
+const ROOM_IMG_4 = CDNImgReturn("gallery", "room-4", "webp");
+const ROOM_IMG_5 = CDNImgReturn("gallery", "room-5", "webp");
+const ROOM_IMG_6 = CDNImgReturn("gallery", "room-6", "webp");
+const ROOM_IMG_7 = CDNImgReturn("gallery", "room-7", "webp");
+const ROOM_IMG_8 = CDNImgReturn("gallery", "room-8", "webp");
+const ROOM_IMG_9 = CDNImgReturn("gallery", "room-9", "webp");
+const ROOM_IMG_10 = CDNImgReturn("gallery", "room-10", "webp");
+const ROOM_IMG_11 = CDNImgReturn("gallery", "room-11", "webp");
+const ROOM_IMG_12 = CDNImgReturn("gallery", "room-12", "webp");
+const ROOM_IMG_13 = CDNImgReturn("gallery", "room-13", "webp");
+const ROOM_IMG_14 = CDNImgReturn("gallery", "room-14", "webp");
+const ROOM_IMG_15 = CDNImgReturn("gallery", "room-15", "webp");
+const ROOM_IMG_16 = CDNImgReturn("gallery", "room-16", "webp");
+const ROOM_IMG_17 = CDNImgReturn("gallery", "room-17", "webp");
+const ROOM_IMG_18 = CDNImgReturn("gallery", "room-18", "webp");
+const ROOM_IMG_19 = CDNImgReturn("gallery", "room-19", "webp");
+const ROOM_IMG_20 = CDNImgReturn("gallery", "room-20", "webp");
+const ROOM_IMG_21 = CDNImgReturn("gallery", "room-21", "webp");
+const ROOM_IMG_22 = CDNImgReturn("gallery", "room-22", "webp");
+const ROOM_IMG_23 = CDNImgReturn("gallery", "room-23", "webp");
+const ROOM_IMG_24 = CDNImgReturn("gallery", "room-24", "webp");
+const ROOM_IMG_25 = CDNImgReturn("gallery", "room-25", "webp");
+const ROOM_IMG_26 = CDNImgReturn("gallery", "room-26", "webp");
+const ROOM_IMG_27 = CDNImgReturn("gallery", "room-27", "webp");
+const ROOM_IMG_28 = CDNImgReturn("gallery", "room-28", "webp");
+const ROOM_IMG_29 = CDNImgReturn("gallery", "room-29", "webp");
+const ROOM_IMG_30 = CDNImgReturn("gallery", "room-30", "webp");
+const ROOM_IMG_31 = CDNImgReturn("gallery", "room-31", "webp");
+const ROOM_IMG_32 = CDNImgReturn("gallery", "room-32", "webp");
+const ROOM_IMG_33 = CDNImgReturn("gallery", "room-33", "webp");
+const ROOM_IMG_34 = CDNImgReturn("gallery", "room-34", "webp");
+const ROOM_IMG_35 = CDNImgReturn("gallery", "room-35", "webp");
+const ROOM_IMG_36 = CDNImgReturn("gallery", "room-36", "webp");
+const ROOM_IMG_37 = CDNImgReturn("gallery", "room-37", "webp");
+const ROOM_IMG_38 = CDNImgReturn("gallery", "room-38", "webp");
+const ROOM_IMG_39 = CDNImgReturn("gallery", "room-39", "webp");
+const ROOM_IMG_40 = CDNImgReturn("gallery", "room-40", "webp");
+const ROOM_IMG_41 = CDNImgReturn("gallery", "room-41", "webp");
+const ROOM_IMG_42 = CDNImgReturn("gallery", "room-42", "webp");
+const ROOM_IMG_43 = CDNImgReturn("gallery", "room-43", "webp");
+const ROOM_IMG_44 = CDNImgReturn("gallery", "room-44", "webp");
+const ROOM_IMG_45 = CDNImgReturn("gallery", "room-45", "webp");
+const ROOM_IMG_46 = CDNImgReturn("gallery", "room-46", "webp");
+const ROOM_IMG_47 = CDNImgReturn("gallery", "room-47", "webp");
+const ROOM_IMG_48 = CDNImgReturn("gallery", "room-48", "webp");
+const ROOM_IMG_49 = CDNImgReturn("gallery", "room-49", "webp");
+const ROOM_IMG_50 = CDNImgReturn("gallery", "room-50", "webp");
 
+export { PLACEHOLDER_GREEN };
 export {
-  PLACEHOLDER,
-  PLACEHOLDER_BLUE,
-  PLACEHOLDER_GREEN,
-  PLACEHOLDER_PURPLE,
-  PLACEHOLDER_RED,
+  OUTSIDE_IMG_1,
+  OUTSIDE_IMG_2,
+  OUTSIDE_IMG_3,
+  OUTSIDE_IMG_4,
+  OUTSIDE_IMG_5,
+  OUTSIDE_IMG_6,
+  OUTSIDE_IMG_7,
+  OUTSIDE_IMG_8,
+  OUTSIDE_IMG_9,
+  OUTSIDE_IMG_10,
+  OUTSIDE_IMG_11,
+  OUTSIDE_IMG_12,
+  OUTSIDE_IMG_13,
+  OUTSIDE_IMG_14,
+  OUTSIDE_IMG_15,
+  OUTSIDE_IMG_16,
+  OUTSIDE_IMG_17,
+  OUTSIDE_IMG_18,
+  OUTSIDE_IMG_19,
+  OUTSIDE_IMG_20,
+  OUTSIDE_IMG_21,
+  OUTSIDE_IMG_22,
+  OUTSIDE_IMG_23,
+  OUTSIDE_IMG_24,
+  OUTSIDE_IMG_25,
+  OUTSIDE_IMG_26,
+  OUTSIDE_IMG_27,
+  OUTSIDE_IMG_28,
 };
-export { MERLIN_1, PHOENIX_1, PHOENIX_2, SOPHIE_1, SOPHIE_2, SOPHIE_3 };
-export { DOG_ART_1, DOG_ART_2 };
+export {
+  ROOM_IMG_1,
+  ROOM_IMG_2,
+  ROOM_IMG_3,
+  ROOM_IMG_4,
+  ROOM_IMG_5,
+  ROOM_IMG_6,
+  ROOM_IMG_7,
+  ROOM_IMG_8,
+  ROOM_IMG_9,
+  ROOM_IMG_10,
+  ROOM_IMG_11,
+  ROOM_IMG_12,
+  ROOM_IMG_13,
+  ROOM_IMG_14,
+  ROOM_IMG_15,
+  ROOM_IMG_16,
+  ROOM_IMG_17,
+  ROOM_IMG_18,
+  ROOM_IMG_19,
+  ROOM_IMG_20,
+  ROOM_IMG_21,
+  ROOM_IMG_22,
+  ROOM_IMG_23,
+  ROOM_IMG_24,
+  ROOM_IMG_25,
+  ROOM_IMG_26,
+  ROOM_IMG_27,
+  ROOM_IMG_28,
+  ROOM_IMG_29,
+  ROOM_IMG_30,
+  ROOM_IMG_31,
+  ROOM_IMG_32,
+  ROOM_IMG_33,
+  ROOM_IMG_34,
+  ROOM_IMG_35,
+  ROOM_IMG_36,
+  ROOM_IMG_37,
+  ROOM_IMG_38,
+  ROOM_IMG_39,
+  ROOM_IMG_40,
+  ROOM_IMG_41,
+  ROOM_IMG_42,
+  ROOM_IMG_43,
+  ROOM_IMG_44,
+  ROOM_IMG_45,
+  ROOM_IMG_46,
+  ROOM_IMG_47,
+  ROOM_IMG_48,
+  ROOM_IMG_49,
+  ROOM_IMG_50,
+};
