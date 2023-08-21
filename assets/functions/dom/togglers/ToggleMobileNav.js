0a4d3a0a2781ad1ac6948184ac98731b6a896c7f
Parker <pp101socials@gmail.com>
2023-08-21 14:16:49 -0400

    Created file

--- assets/functions/dom/togglers/ToggleMobileNav.js
+++ assets/functions/dom/togglers/ToggleMobileNav.js
@@ -6,4 +6,38 @@
 
 import DeclareStorageVariable from "../../data/storage/DeclareStorageVariable";
 
-export default function ToggleMobileNav() {}
+export default function ToggleMobileNav() {
+  DeclareStorageVariable("session", "Mobile Nav Opened", true);
+
+  document.body.style.pointerEvents = "none";
+  document.body.style.overflowY = "hidden";
+
+  // Prevents page from being clickable and scrollable
+  if (document.querySelector(".page")) {
+    document.querySelectorAll(".page").forEach((page) => {
+      page.style.overflowY = "hidden";
+      page.style.pointerEvents = "none";
+    });
+  }
+
+  // Displaying the content
+  document.getElementById("mobileNavMenu").style.display = "block";
+
+  const DARKEN = document.getElementById("mobileNavMenuDarken");
+  const MAIN = document.getElementById("mobileNavMenuMain");
+
+  setTimeout(() => {
+    DARKEN.style.opacity = 1;
+    DARKEN.style.visibility = "visible";
+  }, 700);
+
+  setTimeout(() => {
+    MAIN.style.transform = "translateX(0)";
+  }, 1400);
+
+  setTimeout(() => {
+    DARKEN.style.pointerEvents = "auto";
+    MAIN.style.overflowY = "auto";
+    MAIN.style.pointerEvents = "auto";
+  }, 2500);
+}
