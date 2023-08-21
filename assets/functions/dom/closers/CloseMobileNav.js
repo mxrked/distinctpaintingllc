68ad715bc7468e87c4f06084cb8ca0520bd5e11e
Parker <pp101socials@gmail.com>
2023-08-21 14:16:36 -0400

    Created file

--- assets/functions/dom/closers/CloseMobileNav.js
+++ assets/functions/dom/closers/CloseMobileNav.js
@@ -7,5 +7,34 @@
 import RemoveStorageVariable from "../../data/storage/RemoveStorageVariable";
 
 export default function CloseMobileNav() {
+  const DARKEN = document.getElementById("mobileNavMenuDarken");
+  const MAIN = document.getElementById("mobileNavMenuMain");
 
+  DARKEN.style.pointerEvents = "none";
+  MAIN.style.pointerEvents = "none";
+  MAIN.style.overflowY = "hidden";
+
+  setTimeout(() => {
+    MAIN.style.transform = "translateX(100%)";
+  }, 900);
+
+  setTimeout(() => {
+    DARKEN.style.opacity = 0;
+    DARKEN.style.visibility = "hidden";
+  }, 1200);
+
+  setTimeout(() => {
+    RemoveStorageVariable("session", "Mobile Nav Opened");
+
+    document.body.style.pointerEvents = "auto";
+    document.body.style.overflowY = "auto";
+
+    // Enabling page clicking and scrolling
+    if (document.querySelector(".page")) {
+      document.querySelectorAll(".page").forEach((page) => {
+        page.style.overflowY = "auto";
+        page.style.pointerEvents = "auto";
+      });
+    }
+  }, 2300);
 }
