b11cee275b1d07fa34636bd6a085710e01fa6170
Parker <pp101socials@gmail.com>
2023-08-22 08:11:27 -0400

    Created file

--- assets/data/variables/OBJECTS.js
+++ assets/data/variables/OBJECTS.js
@@ -11,28 +11,32 @@ import {
 const INDEX_TOP_SLIDE_1 = new SliderSlide(
   "I_T001",
   INDEX_TOP_BG_1,
-  "Bringing Homes\nTo Canvas.",
+  "Bringing Homes",
+  "To Canvas.",
   "Expert house painting that revitalizes spaces with vibrant colors and enduring craftsmanship for transformative beauty.",
   "/contact#painting"
 );
 const INDEX_TOP_SLIDE_2 = new SliderSlide(
   "I_T002",
   INDEX_TOP_BG_2,
-  "Revealing Clean\nSurfaces Boldly.",
+  "Revealing Clean",
+  "Surfaces Boldly.",
   "Our pressure washing service rejuvenates surfaces, eliminating dirt and grime to reveal their true splendor.",
   "/contact#pressure_washing"
 );
 const INDEX_TOP_SLIDE_3 = new SliderSlide(
   "I_T003",
   INDEX_TOP_BG_3,
-  "Seamless Walls,\nFlawless Homes.",
+  "Seamless Walls,",
+  "Flawless Homes.",
   "Transforming homes through meticulous drywall repair, we erase imperfections to create seamless, impeccable living spaces.",
   "/contact#drywall_repair"
 );
 const INDEX_TOP_SLIDE_4 = new SliderSlide(
   "I_T004",
   INDEX_TOP_BG_4,
-  "Crafting Dreams,\nBuilding Reality.",
+  "Crafting Dreams,",
+  "Building Reality.",
   "Mastering woodcraft, we shape exquisite designs, from custom furniture to intricate structures, bringing imagination to life.",
   "/contact#carpentry"
 );
