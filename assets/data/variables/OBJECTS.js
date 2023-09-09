45cc8d20caa19649761b0bb5673cc0dbd8696b22
Parker <pp101socials@gmail.com>
2023-09-09 08:59:55 -0400

    Changed top-slide text

--- assets/data/variables/OBJECTS.js
+++ assets/data/variables/OBJECTS.js
@@ -11,32 +11,32 @@ import {
 const INDEX_TOP_SLIDE_1 = new SliderSlide(
   "I_T001",
   INDEX_TOP_BG_1,
-  "Bringing Homes",
-  "To Canvas.",
+  "Greensboro's Best",
+  "Painting Services.",
   "Expert house painting that revitalizes spaces with vibrant colors and enduring craftsmanship for transformative beauty.",
   "/contact#painting"
 );
 const INDEX_TOP_SLIDE_2 = new SliderSlide(
   "I_T002",
   INDEX_TOP_BG_2,
-  "Revealing Clean",
-  "Surfaces Boldly.",
+  "Greensboro's Best",
+  "Pressure Washers.",
   "Our pressure washing service rejuvenates surfaces, eliminating dirt and grime to reveal their true splendor.",
   "/contact#pressure_washing"
 );
 const INDEX_TOP_SLIDE_3 = new SliderSlide(
   "I_T003",
   INDEX_TOP_BG_3,
-  "Seamless Walls,",
-  "Flawless Homes.",
+  "Greensboro's Best",
+  "Drywall Repair.",
   "Transforming homes through meticulous drywall repair, we erase imperfections to create seamless, impeccable living spaces.",
   "/contact#drywall_repair"
 );
 const INDEX_TOP_SLIDE_4 = new SliderSlide(
   "I_T004",
   INDEX_TOP_BG_4,
-  "Crafting Dreams,",
-  "Building Reality.",
+  "Greensboro's Best",
+  "House Carpenters.",
   "Mastering woodcraft, we shape exquisite designs, from custom furniture to intricate structures, bringing imagination to life.",
   "/contact#carpentry"
 );
