abd0cc23a5b049a30872bcaaa020d212f5c2ddbf
Parker <pp101socials@gmail.com>
2023-10-21 09:02:10 -0400

    Added database stuff

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 // React/Next Imports
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 // Library Imports
 
 // Data/Functions/Images Imports
+import { connectDatabase } from "@/db/connections/websiteVisitsCounter_CONNECTION";
 import { TriggerExitAnimations } from "@/assets/functions/dom/triggers/TriggerExitAnimations";
 
 // Component Imports
@@ -25,31 +26,123 @@ import { IndexContact } from "@/assets/components/pages/Index/IndexContact";
 // Style Imports
 import "../assets/styles/modules/Index/Index.module.css";
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async ({ req }) => {
   const INDEX_GALLERY_RES = await fetch(
     "https://raw.githubusercontent.com/mxrked/freelance_projects_CDN/main/distinct-painting-llc/json/INDEX_GALLERY.json"
   );
 
   const INDEX_GALLERY_RES_DATA = await INDEX_GALLERY_RES.json();
 
-  return {
-    props: {
-      index_gallery_data: INDEX_GALLERY_RES_DATA,
-    },
-  };
+  try {
+    // Database activities
+    const DB = await connectDatabase();
+
+    const TOTAL_NUMBER_OF_IPS = await DB.collection("ips").countDocuments();
+
+    const current_ip = req.socket.remoteAddress;
+
+    if (!DB) {
+      return {
+        props: {
+          TOTAL_NUMBER_OF_IPS: 0,
+          current_ip: null,
+          index_gallery_data: null,
+        },
+      };
+    }
+
+    return {
+      props: {
+        TOTAL_NUMBER_OF_IPS,
+        current_ip,
+        index_gallery_data: INDEX_GALLERY_RES_DATA,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      props: {
+        TOTAL_NUMBER_OF_IPS: 0,
+        current_ip: null,
+        index_gallery_data: INDEX_GALLERY_RES_DATA,
+      },
+    };
+  }
 };
 
-export default function Home({ index_gallery_data }) {
+export default function Home({
+  TOTAL_NUMBER_OF_IPS,
+  current_ip,
+  index_gallery_data,
+}) {
   const router = useRouter();
+
+  const [ON_LOCAL_HOST, SET_ON_LOCALHOST] = useState(null);
 
   // Triggering exit animations
   useEffect(() => {
     TriggerExitAnimations();
   }, []);
 
+  //! DB Activities
+  // Checking if connected to DB
+  console.log("Total number of website visits: " + TOTAL_NUMBER_OF_IPS);
+
+  // Triggering getWebsiteVisitsByIps.js
+  useEffect(() => {
+    // Fetching the api route
+    const FETCH_DATA = async () => {
+      try {
+        const response = await fetch("/api/getWebsiteVisitsByIps");
+        const data = await response.json();
+
+        // Handle the data
+        console.log("API response: " + data);
+      } catch (error) {
+        console.error("Error fetching data: " + error);
+      }
+    };
+
+    FETCH_DATA();
+  }, []);
+
+  // Displaying the current website visits when on localhost
+  useEffect(() => {
+    const IPS = ["127.0.0.1", "::1"];
+
+    if (current_ip === IPS[0] || current_ip === IPS[1]) {
+      SET_ON_LOCALHOST(true);
+    }
+  }, []);
+
   return (
     <div id="PAGE" className="overrides_Index full-second">
       <PageHead />
+
+      {ON_LOCAL_HOST && (
+        <div
+          id="websitesVisitCounter"
+          style={{
+            padding: "15px",
+            fontFamily: "sans-serif",
+            backgroundColor: "#e3e3e3",
+            textAlign: "center",
+          }}
+        >
+          <span>
+            Total number of website visits:{" "}
+            <span
+              id="websitesVisitCounterIndicator"
+              style={{ fontWeight: "bold", marginLeft: "10px" }}
+            >
+              {" "}
+              {TOTAL_NUMBER_OF_IPS}{" "}
+            </span>
+          </span>
+        </div>
+      )}
+
       <NavTop disableLink="/" />
       <DesktopNav disableLink="/" />
       <MobileNav disableLink="/" />
